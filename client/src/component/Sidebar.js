69ae0505762aa0ad86f5b901f3dfcb5fcc1523ae
HuynhPoppy <hdhuynh2001262@student.ctuet.edu.vn>
2023-11-06 23:54:11 +0700

    Header,Nav

--- client/src/component/Sidebar.js
+++ client/src/component/Sidebar.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import { apiGetCategories } from "../apis/app";
+import {NavLink} from 'react-router-dom'
 
 const Slidebar = () =>{
+    const [categories, setCategories] = useState(null)
+    const fetchCategories = async () => {
+        const response = await apiGetCategories()
+        if (response.success) setCategories(response.AllProductCategory)
+    }
+    useEffect(()=>{
+        fetchCategories()
+    },[])
+    // console.log(categories)
     return(
         <div>Slidebar</div>
     )
---
+++ client/src/ultils/icon.js
@@ -0,0 +1,16 @@
+import {RiPhoneFill} from 'react-icons/ri'
+import {MdEmail} from 'react-icons/md'
+import {BsFillHandbagFill} from 'react-icons/bs'
+import {FaUserCircle} from 'react-icons/fa'
+import {AiFillHome} from 'react-icons/ai'
+
+
+const icons = {
+    RiPhoneFill,
+    MdEmail,
+    BsFillHandbagFill,
+    FaUserCircle,
+    AiFillHome
+}
+
+export default icons
