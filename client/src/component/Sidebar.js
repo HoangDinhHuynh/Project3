888ba73a482de67b62acd6a1304c1b7700b7ad3f
HuynhPoppy <hdhuynh2001262@student.ctuet.edu.vn>
2023-11-07 22:15:48 +0700

    call api

--- client/src/component/Sidebar.js
+++ client/src/component/Sidebar.js
@@ -1,19 +1,23 @@
 import React,{useState, useEffect} from "react";
 import { apiGetCategories } from "../apis/app";
 import {NavLink} from 'react-router-dom'
+import {createSlug} from '../ultils/helpers' 
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 
 const Slidebar = () =>{
-    const [categories, setCategories] = useState(null)
-    const fetchCategories = async () => {
-        const response = await apiGetCategories()
-        if (response.success) setCategories(response.AllProductCategory)
-    }
-    useEffect(()=>{
-        fetchCategories()
-    },[])
-    // console.log(categories)
+    const {categories} = useSelector(state => state.app)
     return(
-        <div>Slidebar</div>
+        <div className="flex flex-col border">
+            {categories?.map(el => (
+                <NavLink
+                key={createSlug(el.tiltle)}
+                to={createSlug(el.tiltle)}
+                className={({isActive})=> isActive? 'bg-main text-white px-5 pt-[15px] pb[14px] text-sm hover:text-main':'px-5 pt-[15px] pb[14px] text-sm hover:text-main'}
+                >
+                    {el.tiltle}
+                </NavLink>
+            ))}
+        </div>
     )
 }
 
