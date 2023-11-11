c4eda3cc6d44c002e1631a8055704e8e690cfbe3
HuynhPoppy <hdhuynh2001262@student.ctuet.edu.vn>
2023-11-11 18:44:56 +0700

    Feature Complete

--- client/src/component/FeatureProduct.js
+++ client/src/component/FeatureProduct.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {ProductCard} from './'
 import { apiGetProducts} from '../apis'
 
 const FeatureProduct = () =>{
 
+    const [products,setProducts] = useState(null)
+
     const fetchProducts = async () =>{
         const response = await apiGetProducts({limit:9,totalRating:5})
+        console.log(response)
+        if(response.success) setProducts(response.products)
     }
     useEffect(()=>{
         fetchProducts()
@@ -13,6 +17,40 @@ const FeatureProduct = () =>{
     return(
         <div className="w-full">
             <h3 className="text-[20px] font-semibold py-[15px] border-b-2 border-main">FEATURE PRODUCTS</h3>
+            <div className="flex flex-wrap mt-[15px] mx-[-10px]">
+                {products?.map(el => (
+                    <ProductCard 
+                    key={el.id}
+                    image={el.thumb}
+                    tiltle={el.tiltle}
+                    totalRating={el.totalRating}
+                    price={el.price}
+                    />
+                ))}
+            </div>
+            <div className="flex justify-between">
+                <img 
+                src="https://digital-world-2.myshopify.com/cdn/shop/files/banner1-bottom-home2_b96bc752-67d4-45a5-ac32-49dc691b1958_600x.jpg?v=1613166661" 
+                alt=""
+                className="w-[50%] object-contain"
+                />
+                <div className="flex flex-col justify-between w-[24%]">
+                    <img 
+                    src="https://digital-world-2.myshopify.com/cdn/shop/files/banner2-bottom-home2_400x.jpg?v=1613166661" 
+                    alt=""
+                    />
+                    <img 
+                    src="https://digital-world-2.myshopify.com/cdn/shop/files/banner3-bottom-home2_400x.jpg?v=1613166661" 
+                    alt=""
+                    />
+
+                </div>
+                <img 
+                src="https://digital-world-2.myshopify.com/cdn/shop/files/banner4-bottom-home2_92e12df0-500c-4897-882a-7d061bb417fd_400x.jpg?v=1613166661" 
+                alt=""
+                className="w-[24%] object-contain"
+                />
+            </div>
         </div>
     )
 }
--- client/src/component/ProductCard.js
+++ client/src/component/ProductCard.js
@@ -1,9 +1,17 @@
 import React from "react";
+import {formatMoney,renderStarFromNumber} from '../ultils/helpers' 
 
-const ProductCard = () =>{
+const ProductCard = ({price,totalRating,tiltle,image}) =>{
     return(
-        <div>
-           
+        <div className="w-1/3 flex-auto px-[10px] mb-[20px]">
+            <div className="flex w-full border">
+                <img src={image} alt='products' className="w-[90px] object-contain" />
+                <div className="flex flex-col mt-[15px] items-start gap-1 w-full text-xs">
+                    <span className="line-clamp-1 capitalize text-sm">{tiltle?.toLowerCase()}</span>
+                    <span className="flex h-5">{renderStarFromNumber(totalRating, 14)}</span>
+                    <span>{`${formatMoney(price)} VNĐ`}</span>
+                </div>
+            </div>
         </div>
     )
 }
